9188732837e525ed093197985664b7d2e6a47adb
Mike <6721776+mikeeeeeeeeeeeeeeee@users.noreply.github.com>
2022-11-20 08:44:29 +0000

    use argp

--- QuickCodeAnalyzer/QuickCodeAnalyzer.py
+++ QuickCodeAnalyzer/QuickCodeAnalyzer.py
@@ -1,28 +1,16 @@
 import os
-import sys, getopt
+import argparse
+import pathlib
 
 
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv,"hi:",["ifile="])
-    except getopt.GetoptError:
-        print('test.py -i <inputfile>')
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -i <inputfile>')
-            sys.exit()
-        elif opt in ("-i", "--ifile"):
-            rootfolder = arg
+parser = argparse.ArgumentParser(description="QuickCodeAnalyzer")
+parser.add_argument("rootFolder", type=pathlib.Path)
+args = parser.parse_args()
+for subdir, dirs, files in os.walk(args.rootfolder):
+    for filename in files:
+        filepath = subdir + os.sep + filename
 
-    for subdir, dirs, files in os.walk(rootfolder):
-        for filename in files:
-            filepath = subdir + os.sep + filename
-
-            with open(filepath, "r") as fp:
-                classLines = [line for line in fp if "class" in line]
-                for classLine in classLines:
-                    print(classLine)
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+        with open(filepath, "r") as fp:
+            classLines = [line for line in fp if "class" in line]
+            for classLine in classLines:
+                print(classLine)
